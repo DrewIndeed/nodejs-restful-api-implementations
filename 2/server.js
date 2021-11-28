468d44666072223588207c146ee0755b82465b84
Andrew Le <s3820098@rmit.edu.vn>
2021-11-28 12:29:04 +0700

    13. Folder: 2; Commit: add middleware usages and endpoints

--- 2/server.js
+++ 2/server.js
@@ -1,37 +1,46 @@
 // express related
 const express = require("express");
 const app = express();
-const router = express.Router();
+
 // other than express
 const bp = require("body-parser");
 const path = require("path");
 const logger = require("morgan");
 
-// ! REMEMBER TO ADD THIS LINE to add router in express app
-app.use("/", router);
-
-/**
- * ! In order to get access to the post data we have to use body-parser
- * ! Basically what the body-parser is which allows express to read the body
- * ! and then parse that into a Json object that we can understand.
- * ! It parses the HTTP request body.
- */
-let urlencodedParser = bp.urlencoded({ extended: false });
-
-/**
- * * Desired PORT and development environment
- */
+// desired PORT and development environment
 const PORT = process.env.PORT || 8888;
 const NODE_ENV = process.env.NODE_ENV || "development";
-// the 'process' object (A HUGE ONE) provides information about, and control over, the current Node.js process.
-// console.log(process);
-// print out to see what value they get
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
-console.log(PORT);
-console.log(NODE_ENV);
-// setting port and env
-app.set('port', PORT);
-app.set('env', NODE_ENV);
-console.log(process.env.PORT);
-console.log(process.env.NODE_ENV);
+app.set("port", PORT);
+app.set("env", NODE_ENV);
+
+// use morgan and body parser
+app.use(logger("tiny"));
+app.use(bodyParser.json());
+
+// setting routes as default file when the app first starts
+app.use("/", require(path.join(__dirname, "routes")));
+
+// endpoints to handle error of code: 404 and 500
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} Not Found`);
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+// server port listener - port 8888
+app.listen(PORT, () => {
+  console.log(
+    `Express Server started on Port ${app.get(
+      "port"
+    )} | Environment : ${app.get("env")}`
+  );
+});
