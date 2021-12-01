18d0e70d3e33206b607c56c39caaf4892a7683a0
Andrew Le <s3820098@rmit.edu.vn>
2021-12-01 16:23:59 +0700

    30. Folder: 6; Commit: add user by id

--- 6/services/routes.js
+++ 6/services/routes.js
@@ -19,6 +19,30 @@ const getAllUsers = async (req, res, next) => {
   });
 };
 
+const getUserById = async (req, res, next) => {
+  let sql = "select * from user where id = ?";
+  let params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    if (!row) {
+      res
+        .status(404)
+        .json({ error: `User with id = ${req.params.id} Not Found!` });
+      return;
+    }
+
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+};
+
 // create the route (the URL) and attach the method to it
 router.route("/api/users").get(getAllUsers);
+router.route("/api/users/:id").get(getUserById);
 module.exports = router;
