25299c4ffe4e83e6669725a84034d530131a7832
Andrew Le <s3820098@rmit.edu.vn>
2021-12-01 13:57:23 +0700

    25. Folder: 6; Commit: add some config and initializations

--- 6/server.js
+++ 6/server.js
@@ -1,1 +1,45 @@
-console.log("Hello World!");
+// express related
+const express = require("express");
+const app = express();
+
+// other than express
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// desired PORT and development environment
+const PORT = process.env.PORT || 6868;
+const NODE_ENV = process.env.NODE_ENV || "development";
+app.set("port", PORT);
+app.set("env", NODE_ENV);
+
+// use body parser (to handle POST methods)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// setting routes as default folder when the app first starts
+app.use("/", require(path.join(__dirname, "./routes.js")));
+
+// endpoints to handle error of code: 404 and 500
+app.use((req, res, next) => {
+  const err = new Error(`${req.method} ${req.url} Not Found`);
+  err.status = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  console.log("Manual error printing:", err.message);
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message,
+    },
+  });
+});
+
+// server port listener - port 6868
+app.listen(PORT, () => {
+  console.log(
+    `Express Server started on Port ${app.get(
+      "port"
+    )} | Environment : ${app.get("env")}`
+  );
+});
